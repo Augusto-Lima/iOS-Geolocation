60b39cb2f23caa18a0ef1f870c524cb573a646db
Augusto Lima <Augusto@Augustos-Mac.local>
2015-09-04 11:52:03 -0700

    Initial Commit

---
+++ GeolocationExamples/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  GeolocationExamples
+//
+//  Created by Augusto Lima on 9/4/15.
+//  Copyright (c) 2015 Augusto Lima. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ GeolocationExamples/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  GeolocationExamples
+//
+//  Created by Augusto Lima on 9/4/15.
+//  Copyright (c) 2015 Augusto Lima. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
